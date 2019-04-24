52805d4e6b61bc5b2ff0c4cb425ffad956fef7df
snahian <contactsajednahian@gmail.com>
2019-04-24 13:40:34 -0400

    init commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:oeTIlXqdYQw4kIC1@mern-7jkbh.mongodb.net/devconnector?retryWrites=true"
+};
---
+++ routes/api/profile.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// @route GET api/posts/test
+// @desc Tests post router
+// @access Public
+router.get("/test", (req, res) => {
+  res.json({ msg: "Profile works" });
+});
+
+module.exports = router;
